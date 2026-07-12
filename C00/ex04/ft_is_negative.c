a30eaaf0f24124b6efecb7a684efb6b54110588c
elghazouani fati <elghazouani4b@gmail.com>
2026-07-12 16:19:14 +0100

    ft_is_negative

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -1,12 +1,24 @@
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_is_negative(int n)
 {
-	char	c;
-
-	if (n >= 0)
-		c = 'P';
+	if (n < 0)
+		ft_putchar('N');
 	else
-		c = 'N';
-	write (1, &c, 1);
+		ft_putchar('P');
 }
+/*
+int	main()
+{
+	ft_is_negative(-4);
+	ft_putchar('\n');
+	ft_is_negative(9);
+	ft_putchar('\n');
+	ft_is_negative(0);
+}
+*/
