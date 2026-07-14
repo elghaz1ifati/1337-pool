c6aad21e1648a763610d2cef43932eb13723d0ba
elghazouani fati <elghazouani4b@gmail.com>
2026-07-15 00:41:52 +0100

    ft_strcpy

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main()
+{
+	char dest[10];
+	char *src = "hello";
+	char *res;
+
+	res = ft_strcpy(dest, src);
+	printf("Src : %s\n", src);
+	printf("Adrss of src : %p\n", (void*)src);
+	printf("Des : %s\n", dest);
+	printf("Adrss of dest : %p\n", (void*)dest);
+	printf("Res : %s\n", res);
+	printf("Adrss of res : %p", (void*)res);
+}
+*/
