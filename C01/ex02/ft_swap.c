2fdcf4c3cdad22ffab845829ee608a48e8edea22
elghazouani fati <elghazouani4b@gmail.com>
2026-07-16 22:52:01 +0100

    ft_swap

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
@@ -8,3 +9,12 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = swp;
 }
+/*
+int	main()
+{
+	int	n1 = 1;
+	int	n2 = 2;
+	ft_swap(&n1, &n2);
+	printf("n1 = %d\nn2 = %d", n1, n2);
+}
+*/
