cb2ff002bc488ce0427c8409c1b10019ca476f12
elghazouani fati <elghazouani4b@gmail.com>
2026-07-16 23:27:02 +0100

    ft_strlen

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -1,14 +1,21 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
-		str++;
 	}
 	return (i);
 }
+/*
+int	main()
+{
+	char str[] = "EL GHAZOUANI Fatima ezzahra";
+	printf("%d", ft_strlen(str));
+}
+*/
