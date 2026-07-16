e516e096470617b995957a32f3ddffa5c20021eb
elghazouani fati <elghazouani4b@gmail.com>
2026-07-16 23:11:25 +0100

    ft_ultimate_div_mod

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
@@ -11,3 +12,16 @@ void	ft_ultimate_div_mod(int *a, int *b)
 		*a = tmp;
 	}
 }
+/*
+int main ()
+{
+	int	n1 = 42;
+	int	n2 = 2;
+	
+	printf("n1 = %d\n", n1);
+	printf("n2 = %d\n", n2);
+	ft_ultimate_div_mod(&n1, &n2);
+	printf("div = %d\n",n1);
+	printf("mod = %d\n", n2);
+}
+*/
