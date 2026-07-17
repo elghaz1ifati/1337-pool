64c9adb89a31e801ab0de5b18836e7e8ab75b8bb
elghazouani fati <elghazouani4b@gmail.com>
2026-07-17 17:52:44 +0100

    ft_rev_int_tab

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,19 +1,50 @@
-#include <unistd.h>
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	i;
 	int	e;
-	int	tmp;
 
 	i = 0;
-	e = size -1;
+	e = size - 1;
 	while (i < e)
 	{
-		tmp = tab[i];
-		tab[i] = tab [e];
-		tab[e] = tmp;
+		ft_swap(&tab[i], &tab[e]);
 		i++;
 		e--;
 	}
 }
+/*
+int	main()
+{
+	int	tab[] = {10, 20, 30, 40, 50};
+	int	size = 5;
+	int	i;
+
+	printf("Before reverse : ");
+	i = 0;
+	while(i < size)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+
+	i = 0;
+	printf("\nAfter reverse : ");
+	ft_rev_int_tab(tab, size);
+	while(i < size)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+}
+*/
