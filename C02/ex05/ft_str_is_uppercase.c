29233a302ceeaf583ab7e8ceb3fbf98d1601b744
elghazouani fati <elghazouani4b@gmail.com>
2026-07-20 17:05:35 +0100

    ft_str_is_uppercase

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	main()
+{
+	printf("%d\n", ft_str_is_uppercase("HELLO"));
+	printf("%d\n",ft_str_is_uppercase("hello"));
+	printf("%d", ft_str_is_uppercase(""));
+}
