0ff68b7d4127c6151c4d340419afc094e1883d0a
elghazouani fati <elghazouani4b@gmail.com>
2026-07-20 16:01:03 +0100

    ft_str_is_lowercase

---
+++ C02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+/*
+int	main()
+{
+	printf("%d\n", ft_str_is_lowercase("hu4958"));
+	printf("%d\n", ft_str_is_lowercase("HYT"));
+	printf("%d\n", ft_str_is_lowercase("hahahaha"));
+	printf("%d", ft_str_is_lowercase(""));
+}
+*/	
