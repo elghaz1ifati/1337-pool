3ab9b301e3347d5fa244e9cb1bc67048c0a1b014
elghazouani fati <elghazouani4b@gmail.com>
2026-07-20 13:46:00 +0100

    ft_strncpy

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,8 +1,26 @@
-#include <unistd.h>
+#include <stdio.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
-	while()
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
+/*
+int main()
+{
+	char	*str = "Hello";
+	int	n = 2;
+	char	dest[10];
+
+	printf("%s", ft_strncpy(dest, str, n));
+}
+*/
+//n.b: wa can't compare an int with an unsigned int
