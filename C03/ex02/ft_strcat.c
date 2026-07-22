ec16ad5a21ec3d075cc6152a0c3a8ba3d0a45e22
elghazouani fati <elghazouani4b@gmail.com>
2026-07-22 15:54:13 +0100

    ft_strcat

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main()
+{
+	char	dest[30] = "hello";
+	char	src[] = " world";
+
+	printf("%s\n", dest);
+	ft_strcat(dest, src);
+	printf("%s", dest);
+}
+*/
