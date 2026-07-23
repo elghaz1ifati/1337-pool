db725d2c0eb6e34c352d1bd26a5048b896329da6
elghazouani fati <elghazouani4b@gmail.com>
2026-07-23 15:13:09 +0100

    ft_strstr

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (to_find[0] == '\0')
+		return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
+	}
+	return (0);
+}
+/*
+int	main()
+{
+	char str[] = "hello kali linux";
+	char to_find [] = "kali";
+	printf("%s", ft_strstr(str, to_find));
+
+}
+*/
