7a630879f29dceacba28f70231d8aeccfc38ba35
elghazouani fati <elghazouani4b@gmail.com>
2026-07-23 20:18:57 +0100

    ft_atoi

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	n;
+	int	s;
+
+	i = 0;
+	n = 0;
+	s = 1;
+	while (str[i] == ' ' || str[i] >= '\t' && str[i] <= '\r')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			s *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + (str[i] - '0');
+		i++;
+	}
+	return (n * s);
+}
+/*
+int	main()
+{
+	printf("%d", ft_atoi("	---+--+1234ab567"));
+}
+*/
