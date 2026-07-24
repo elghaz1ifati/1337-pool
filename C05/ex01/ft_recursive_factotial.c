926a19a4f1bcb5d93c3b1300e54234e806187190
elghazouani fati <elghazouani4b@gmail.com>
2026-07-24 22:34:24 +0100

    ft_recursive_factorial

---
+++ C05/ex01/ft_recursive_factotial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if  (nb <= 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+int	main()
+{
+	printf("%d", ft_recursive_factorial(3));
+}
